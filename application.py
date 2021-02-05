5d210e177fe7580ea5255d8bd0a00cd9ad082850
SergiGan <sj.shaynyuk@gmail.com>
2021-02-05 23:02:35 +0200

    1.6, use the master branch, not the main

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ def root():
 # 
 @application.route("/hello")
 def index():
-  return "Hello World from Flask App Page.<b> v1.0"
+  return "Hello World from Flask App Page.<b> v1.1"
 
 #--------Main------------------
 if __name__ == "__main__":
